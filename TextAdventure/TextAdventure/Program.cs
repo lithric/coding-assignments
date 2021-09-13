51c5ba08fc21ed1ccd998c03d5c49c906a9b7ce7
lithric <eliongotswag@gmail.com>
2021-09-13 10:44:52 -0400

    comit

---
+++ TextAdventure/TextAdventure/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex rx = new Regex(@"");
+            StreamReader sr = new StreamReader("./Story.txt");
+            string line = sr.ReadToEnd();
+            // file.ReadAllText(Regex);
+            Console.WriteLine(line);
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
