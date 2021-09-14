d4132237934b0e522ead8e5624f4143996c042d6
lithric <eliongotswag@gmail.com>
2021-09-14 10:43:46 -0400

    grieojg

--- TextAdventure/TextAdventure/Program.cs
+++ TextAdventure/TextAdventure/Program.cs
@@ -10,14 +10,36 @@ namespace TextAdventure
 {
     class Program
     {
+        public static StreamReader StoryFile = new StreamReader("../../Story.txt");
+        public static string StoryScript = StoryFile.ReadToEnd();
+        public static string GetStoryById(string StoryID="00.00.00") {
+            Regex rx = new Regex($@"%StoryID%{StoryID}\:(?:\r\n|\r|\n)*((?:[^%]|\r\n|\r|\n)*)(?:\r\n|\r|\n)%End%");
+            Match m = rx.Match(StoryScript);
+            if (!m.Success)
+            {
+                return "StoryID Not Found";
+            }
+            return m.Groups[1].Value;
+        }
+        public static List<string> GetStoriesByPage(string StoryPage) {
+            Regex rx = new Regex($@"(?<=%StoryPage%{StoryPage}\*(?:[^\*]|\r\n|\r|\n)*)%StoryID%(?:.*)\:(?:\r\n|\r|\n)*((?:[^%]|\r\n|\r|\n)*)(?:\r\n|\r|\n)%End%");
+            MatchCollection m = rx.Matches(StoryScript);
+            List<string> returnValue = new List<string>();
+            for (int i=0; i<m.Count; i++)
+            {
+                returnValue.Add(m[i].Groups[1].Value);
+            }
+            return returnValue;
+        }
+        public static List<List<string>> GetStoriesByBook(string StoryBook) {
+            Regex rx = new Regex($@"(?<=%StoryPage%(.*)\*(?:[^\*]|\r\n|\r|\n)*)%StoryID%(?:.*)\:(?:\r\n|\r|\n)*((?:[^%]|\r\n|\r|\n)*)(?:\r\n|\r|\n)%End%");
+            return new List<List<string>>();
+        }
         static void Main(string[] args)
         {
-            Regex rx = new Regex(@"");
-            StreamReader sr = new StreamReader("./Story.txt");
-            string line = sr.ReadToEnd();
-            // file.ReadAllText(Regex);
-            Console.WriteLine(line);
-            sr.Close();
+            StoryFile.Close();
+            List<string> test = GetStoriesByPage("Intro");
+            Console.WriteLine(test[1]);
             Console.ReadLine();
         }
     }
