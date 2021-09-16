54db9d3e26f173f97513b542d4a824f87b6b16f5
lithric <eliongotswag@gmail.com>
2021-09-16 10:33:13 -0400

    added literal type

--- TextAdventure/TextAdventure/Program.cs
+++ TextAdventure/TextAdventure/Program.cs
@@ -5,13 +5,143 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
+public struct Literal
+{
+    public int integ;
+    public string str;
+    public Type type;
+    public Literal(object val)
+    {
+        string aura = val.GetType().ToString();
+        switch (aura)
+        {
+            case "System.Int32":
+                integ = (int)val;
+                str = val.ToString();
+                break;
+            case "System.String":
+                integ = Int32.TryParse((string)val, out int temp) ? temp:0;
+                str = (string)val;
+                break;
+            default:
+                integ = 0;
+                str = "";
+                break;
+        }
+        type = val.GetType();
+    }
 
+    public static implicit operator Literal(int x)
+    {
+        return new Literal(x);
+    }
+    public static implicit operator Literal(string x)
+    {
+        return new Literal(x);
+    }
+    public static implicit operator bool(Literal x)
+    {
+        if (x.integ != 0)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+    public static Literal operator ==(Literal x, object y)
+    {
+        string typeValue = y.GetType().ToString();
+        switch (typeValue)
+        {
+            case "System.Int32":
+                if (x.integ == (int)y)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            case "System.Int32[]":
+                if (x.integ == ((int[])y)[0] && ((int[])y).Length == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            case "System.String":
+                if (x.str == (string)y)
+                {
+                    return 1;
+                } 
+                else
+                {
+                    return 0;
+                }
+            case "System.String[]":
+                if (x.str == ((string[])y)[0] && ((string[])y).Length == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            default:
+                return 0;
+        }
+    }
+    public static Literal operator !=(Literal x, object y)
+    {
+        string typeValue = y.GetType().ToString();
+        switch (typeValue)
+        {
+            case "System.Int32":
+                if (x.integ != (int)y)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            case "System.Int32[]":
+                if (x.integ != ((int[])y)[0] && ((int[])y).Length == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            case "System.String":
+                if (x.str != (string)y)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            case "System.String[]":
+                if (x.str != ((string[])y)[0] && ((string[])y).Length == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            default:
+                return 1;
+        }
+    }
+}
 namespace TextAdventure
 {
-    public struct Literal
-    {
-        public int x;
-    }
     class Program
     {
         public static StreamReader StoryFile = new StreamReader("../../Story.txt");
@@ -40,8 +170,15 @@ namespace TextAdventure
             return new List<List<string>>();
         }
         static void Main(string[] args) {StoryFile.Close();
-            List<string> test = GetStoriesByPage("Intro");
-            Console.WriteLine(test[1]);
+            //List<string> test = GetStoriesByPage("Intro");
+            //Console.WriteLine(test[1]);
+            Literal num = "5";
+            int[] a = { 5 };
+            if (num == 5)
+            {
+                Console.WriteLine("true");
+            }
+            Console.WriteLine(num.integ);
             Console.ReadLine();
         }
     }
