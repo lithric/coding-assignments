e191c82c9ad425b5e3b51eebd807f80041878c66
lithric <35087325+lithric@users.noreply.github.com>
2021-09-23 11:19:19 -0400

    Update Story.cs

--- NerdopolyProject/NerdopolyProject/Story.cs
+++ NerdopolyProject/NerdopolyProject/Story.cs
@@ -31,7 +31,7 @@ namespace NerdopolyProject
             {
                 nestName = "";
             }
-            Regex rx1 = new Regex($@"%Story(?:{nestName})+([^ \r\n\t])*%*({nestLabel})({denote})[\r\n]((?!.*%Story.*%.*[{higherText}]).*[^{higherText}]|[\r\n])*[^\r\n](?![^\r\n])");
+            Regex rx1 = new Regex($@"%Story(?:{nestName})+([^ \r\n\t])*%*({nestLabel})({denote})[\r\n]((?:(?!.*%Story.*%.*[{higherText}]).*[^{higherText}]|[\r\n])*[^\r\n])(?![^\r\n])");
             MatchCollection m1 = rx1.Matches(StoryScript);
             Console.WriteLine(m1[0]);
             return "";
