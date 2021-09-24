2c3eec59aa6845ee4a3289ad9048788d282330e5
lithric <eliongotswag@gmail.com>
2021-09-24 10:43:01 -0400

    done stuff

--- NerdopolyProject/NerdopolyProject/Story.cs
+++ NerdopolyProject/NerdopolyProject/Story.cs
@@ -8,6 +8,20 @@ using System.IO;
 
 namespace NerdopolyProject
 {
+    class StoryObject
+    {
+        public string name;
+        public string label;
+        public string denote;
+        public string text;
+        public StoryObject(Match m)
+        {
+            name = m.Groups[1].Value == "%" ? "" : m.Groups[1].Value;
+            label = m.Groups[2].Value;
+            denote = m.Groups[3].Value;
+            text = m.Groups[4].Value.Substring(1);
+        }
+    }
     class Story
     {
         /*
@@ -16,6 +30,7 @@ namespace NerdopolyProject
             return new Regex($@"{pre}{denote}([^{denote}]|\n)*(?!.*{denote})");
         }
         */
+        //Ch1[0](1)
         public static string[] SectionSymbolList = { @"\:", @"\*", @"\^", @"\#", @"\$", @"\@" };
         public static StreamReader StoryFile = new StreamReader("../../Story.txt");
         public static string StoryScript = StoryFile.ReadToEnd();
@@ -31,9 +46,12 @@ namespace NerdopolyProject
             {
                 nestName = "";
             }
-            Regex rx1 = new Regex($@"%Story(?:{nestName})+([^ \r\n\t])*%*({nestLabel})({denote})[\r\n]((?!.*%Story.*%.*[{higherText}]).*[^{higherText}]|[\r\n])*[^\r\n](?![^\r\n])");
+            Regex rx1 = new Regex($@"%Story(?:{nestName})+([^ \r\n\t])*%*({nestLabel})({denote})[\r\n]((?:(?!.*%Story.*%.*[{higherText}]).*[^{higherText}]|[\r\n])*[^\r\n])(?![^\r\n])");
             MatchCollection m1 = rx1.Matches(StoryScript);
-            Console.WriteLine(m1[0]);
+            //returnValue(0) = new StoryObject(m1[0]);
+            //returnValue(0).label = (new StoryObject(m1[0])).label;
+            Func<int, StoryObject> returnValue = (x) => { return new StoryObject(m1[x]); };
+            Console.WriteLine(returnValue(1).text);
             return "";
         }
     }
