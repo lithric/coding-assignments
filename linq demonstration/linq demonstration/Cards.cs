3c6406d9d0245115752da15cc5ebfd1f4b9779d5
Oliver_Elion <Oliver_Elion@student.mahoningctc.com>
2021-10-20 10:41:46 -0400

    found stuff

---
+++ linq demonstration/linq demonstration/Cards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_demonstration
+{
+    struct Cards
+    {
+        public static void Add(List<(string, string, string, string)> deck)
+        {
+            Func<int, (string, string, string, string)> bank = (int x) => {return deck[x];};
+        }
+    }
+}
--- linq demonstration/linq demonstration/Program.cs
+++ linq demonstration/linq demonstration/Program.cs
@@ -9,7 +9,7 @@ namespace linq_demonstration
     class Program
     {
         public static int persist = 20;
-        static IEnumerable<string> Pins()
+        static IEnumerable<string> Pins() // 3
         {
             yield return "rare";
             yield return "marb";
@@ -38,7 +38,7 @@ namespace linq_demonstration
             yield return "hearts";
             yield return "spades";
         }
-        public static List<(string, string, string, string)> Manifest(string name)
+        public static List<(string, string, string, string)> Manifest(string name) // creates the list of cards with the deck name and returns it as a list of tuples
         {
             List<(string, string, string, string)> returnValue = new List<(string, string, string, string)>();
             var deck = from p in Pins()
@@ -47,23 +47,49 @@ namespace linq_demonstration
                        select new { Pin = p, Rank = r, Suit = s };
             foreach (var card in deck)
             {
-                returnValue.Add((card.Pin, card.Rank, card.Suit, name));
+                returnValue.Add((card.Pin, card.Rank, card.Suit,name));
             }
             return returnValue;
         }
+        public static void DataUpdate(ref List<Func<int, (string, string, string, string)>> data, string name) {
+            Func<int, (string, string, string, string)> newData = (int x) => { return Manifest(name)[x]; };
+            data.Add(newData);
+        }
         static void Main(string[] args)
         {
-            List<(string, string, string, string)> deck = Manifest("delta");
-            List<int> bob = CreateList();
-            Console.WriteLine(bob[0]);
-            Console.WriteLine(bob[0]);
+            // list that executes function
+                List<Func<int>> bob = CreateList();
+                Console.WriteLine(bob[0]());
+                Console.WriteLine(bob[0]());
+            // list that executes function
+
+            //the card company problem
+            var database = new Dictionary<string, List<(string, string, string, string)>>();
+            for (int i=0; i<100; i++) // 156,000 elements in database
+            {
+                database.Add(i.ToString(),Manifest(i.ToString()));
+            }
+            // the goal is to reduce the numbers in database while keeping the same data available
+            // data[0](0).item1
+            // data[deck number](card number).cardInfo
+            delegate var complexData = (from d in database group d.Value by d.Key).ToDictionary(p => p);
+            Console.WriteLine(complexData);
+            List < Func<int, (string, string, string, string)> > data = (from d in database select (Func<int, (string, string, string, string)>)((int x) => { return Manifest(d.Key)[x]; })).ToList();
+            // 100x less data stored in the object "data"
+            DataUpdate(ref data, "120");
+
+            //the card company problem
             Console.ReadLine();
         }
-        public static List<object> CreateList()
+        public static int ListFunc()
         {
-            List<Delegate> bob = new List<Delegate> { ListFunc };
+            persist++;
+            return persist;
+        }
+        public static List<Func<int>> CreateList()
+        {
+            List<Func<int>> bob = new List<Func<int>> { ListFunc };
             return bob;
         }
-        public delegate int ListFunc();
     }
 }
